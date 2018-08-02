28aea06d89d09d447b79ba610bb58a038ca7fdcc
Irtaza Abbas <irtazabbas@gmail.com>
2018-08-02 17:09:50 +0500

    update - controls

--- src/areas/todoer/components/controls/controls.js
+++ src/areas/todoer/components/controls/controls.js
@@ -7,22 +7,21 @@ export default props => {
   return (
     <header className="mdc-top-app-bar mdc-top-app-bar--short controls">
       <div className="mdc-top-app-bar__row">
-        <section className="mdc-top-app-bar__section
-          mdc-top-app-bar__section--align-end" role="toolbar">
-          <a href="#" className="material-icons mdc-top-app-bar__action-item">
+        <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+          <a className="material-icons mdc-top-app-bar__action-item">
             <Icon icon='waves' />
           </a>
-          <a href="#" className="material-icons mdc-top-app-bar__action-item">
+          <a className="material-icons mdc-top-app-bar__action-item">
             <Icon icon='clear' />
           </a>
-          <a href="#" className="material-icons mdc-top-app-bar__action-item">
+          <a className="material-icons mdc-top-app-bar__action-item">
             <Icon icon='create' />
           </a>
-          <a href="#" className="material-icons mdc-top-app-bar__action-item">
+          <a className="material-icons mdc-top-app-bar__action-item">
             <Icon icon='next_week' />
           </a>
         </section>
       </div>
     </header>
-  )
+  );
 }
