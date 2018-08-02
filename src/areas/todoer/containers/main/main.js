4ecf884f312031dd56442abd28287aa52252380f
Irtaza Abbas <irtazabbas@gmail.com>
2018-08-02 17:09:00 +0500

    update - todoer main

--- src/areas/todoer/containers/main/main.js
+++ src/areas/todoer/containers/main/main.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 import Aux from '../../../../hocs/aux';
 import Controls from '../../components/controls/controls';
+import Doables from '../doables/doables';
 
 export default class TodoerMain extends Component {
   render() {
     return (
       <Aux>
         <Controls />
+
+        <Doables />
       </Aux>
     );
   }
