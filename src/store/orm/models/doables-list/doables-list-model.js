11ae240b1e9c48139ec7465a95626542607a9692
Irtaza Abbas <irtazabbas@gmail.com>
2018-08-30 16:16:56 +0500

    update - doables list model to extend BaseMode, a  new selector

--- src/store/orm/models/doables-list/doables-list-model.js
+++ src/store/orm/models/doables-list/doables-list-model.js
@@ -1,15 +1,22 @@
-import { Model, attr, fk } from 'redux-orm';
+import { attr, fk } from 'redux-orm';
 
+import { BaseModel } from '../base-model';
 import { creators } from './doables-list-actions';
 import reducer from './doables-list-reducer';
 import { modelNames } from '../dictionary';
 
-export class DoablesList extends Model {
+export class DoablesList extends BaseModel {
   static reducer = reducer;
 
   static add(dispatch) {
     dispatch(creators.addDoable());
   }
+
+  static all_sel = [
+    session => {
+      return session.doablesList.all().toRefArray();
+    }
+  ]
 }
 
 DoablesList.modelName = modelNames.doablesList;
