0afbc4003b0ec0794665fc33d01a4f500cf66bb1
Irtaza Abbas <irtazabbas@gmail.com>
2018-09-18 17:31:20 +0500

    update - list in details drawer

--- src/modules/todoer/containers/details-drawer/details-drawer.js
+++ src/modules/todoer/containers/details-drawer/details-drawer.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 
 import Head from './head/head';
 import { DoableModel } from '../../../models-ref';
+import List from '../list/list';
+import DoableAdder from '../../components/doable-adder/doable-adder';
 
 import './details-drawer.scss';
 
@@ -10,6 +12,18 @@ class DetailsDrawer extends Component {
   state = {
     editingTitle: false,
     editingDescription: false
+  }
+
+  addDoable = title => {
+    this.props.addDoable(title, this.props.doable.id);
+  }
+
+  updateTitle = title => {
+    this.props.updateTitle(this.props.doable.id, title);
+  }
+
+  remove = () => {
+    this.props.remove(this.props.doable.id);
   }
 
   render() {
@@ -29,11 +43,16 @@ class DetailsDrawer extends Component {
           doableId={ this.props.doable.id }
         />
         <hr/>
+        <div className="body">
+          <List doables={ this.props.doable.doables }
+            doableClicked={ this.props.doableClicked }>
+
+            <DoableAdder addDoable={ this.addDoable } />
+            
+          </List>
+        </div>
         {/* TODO: do the close button properly */}
         <button onClick={ this.props.close }>close</button>
-        <div className="body">
-          
-        </div>
       </aside>
     );
   }
@@ -43,5 +62,17 @@ const mapStateToProps = state => ({
   doable: DoableModel.getSelected_sel(state)
 });
 
+const mapDispatchToProps = dispatch => {
+  return {
+    updateTitle: (id, title) => DoableModel.updateTitle(
+      dispatch, id, title
+    ),
+    addDoable: (title, doableId) => DoableModel.addToDoable(
+      dispatch, doableId, title
+    ),
+    remove: id => DoableModel.remove(dispatch, id)
+  }
+};
 
-export default connect(mapStateToProps, null)(DetailsDrawer);
+
+export default connect(mapStateToProps, mapDispatchToProps)(DetailsDrawer);
